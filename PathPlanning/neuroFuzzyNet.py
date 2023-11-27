8ef7dea4a01006d845391cc23066679c4ecead3d
Dylan Tribble <70781395+tremblay17@users.noreply.github.com>
2023-11-27 10:17:32 -0600

    Init files

---
+++ PathPlanning/neuroFuzzyNet.py
@@ -0,0 +1,15 @@
+#TODO: Implement class for neural network to improve fuzzy system
+
+class NueroFuzzyNet:
+    def __init__(self) -> None:
+        pass
+    def layers(self):
+        pass
+    def model(self):
+        pass
+    def modelOutput(self):
+        pass
+    def modelInput(self):
+        pass
+    def modelTrain(self):
+        pass
